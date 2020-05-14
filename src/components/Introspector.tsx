214e11912d9eb0f43fffccce320aff0c81af58e7
Matthew Belford <mattbford2@gmail.com>
2020-05-14 17:30:43 -0600

    fix: remove mock test file

--- src/components/Introspector.tsx
+++ src/components/Introspector.tsx
@@ -12,10 +12,8 @@ interface IProps {
 }
 
 const Introspector: React.FC<IProps> = (props) => {
-    // get listener traffic if no logs were passed in
-    if (props.logs === undefined || props.logs.length === 0) {
-        const [history] = useWebRequest();
-    }
+    // get listener traffic
+    const [history] = useWebRequest();
 
     return(
         <div>
--- src/containers/MyApp.tsx
+++ src/containers/MyApp.tsx
@@ -11,14 +11,13 @@ const MyApp: React.FC = () => {
   const darkMode = useDarkMode();
   const theme = darkMode.value ? darkTheme : lightTheme;
 
-  /*const logs: IJSONRPCLog[] = [{
+  const logs: IJSONRPCLog[] = [{
     timestamp: new Date(),
     payload: {
         jsonrpc: "2.0",
         method: "foo",
     },
-  }];*/
-  const logs: IJSONRPCLog[] = [];
+  }];
 
   // Create devtools panel for introspector extension
   chrome.devtools.panels.create("Introspector",
