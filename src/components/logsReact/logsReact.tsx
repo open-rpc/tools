7b36afe187651a6eaaaff90692fc26680097dd7f
Matt Belford <mattbford2@gmail.com>
2020-07-03 12:59:46 -0600

    fix: make logs-react component fit div when imported

--- src/components/logsReact/logsReact.tsx
+++ src/components/logsReact/logsReact.tsx
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme: Theme) =>
     logsReact: {
       display: "flex",
       width: "100%",
-      height: "100vh",
+      height: "100%",
     },
     extendDiv: {
       width: "15px",
