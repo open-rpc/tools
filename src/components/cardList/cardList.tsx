1815d5f68a14e05c0f684be343528d1b6ded7731
shanejonas <jonas.shane@gmail.com>
2020-10-14 20:22:18 -0700

    fix: overflow for cardlist
    - load sample logs when loaded in browser

--- src/components/cardList/cardList.tsx
+++ src/components/cardList/cardList.tsx
@@ -11,20 +11,16 @@ interface IProps {
 
 const CardList: React.FC<IProps> = (props) => {
 
-  const cardRender = (call, i) => {
-    if (props.logs.length - 1 === i) {
-      return <CardListItem log={call} filter={props.filter} open={true} />;
-    } else {
-      return <CardListItem log={call} filter={props.filter} open={false} />;
-    }
-  };
-
   return (
-    <div style={{ width: "100%", height: "100%", overflow: "auto" }}>
-      {props.logs.map((call, i) => (
-        cardRender(call, i)
-      ))}
-    </div>
+    <>
+      {props.logs.map((call, i) => {
+        if (props.logs.length - 1 === i) {
+          return <CardListItem log={call} filter={props.filter} open={true} />;
+        } else {
+          return <CardListItem log={call} filter={props.filter} open={false} />;
+        }
+      })}
+    </>
   );
 };
 
