6addf873e794cdf06c72ff1276bea672d43ba6a5
Zachary Belford <belfordz66@gmail.com>
2026-02-17 13:49:11 -0800

    feat(extensions): add x-implemented-by extension

---
+++ .changeset/purple-buckets-wonder.md
@@ -0,0 +1,6 @@
+---
+"@open-rpc/extensions": patch
+---
+
+Add the `x-implemented-by` OpenRPC extension to `@open-rpc/extensions`, including
+its JSON definition, TypeScript exports, and documentation.
--- packages/extensions/src/index.ts
+++ packages/extensions/src/index.ts
@@ -1,2 +1,4 @@
 export { XErrorGroupsJSON } from './x-error-groups';
 export type { XErrorGroupType } from './x-error-groups';
+export { XImplementedByJSON } from './x-implemented-by';
+export type { XImplementedByType } from './x-implemented-by';
